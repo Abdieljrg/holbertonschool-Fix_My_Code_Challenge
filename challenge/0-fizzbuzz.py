735e457a3e82637ac7c31728472aec4bb3761194
Abdiel j. Rodríguez <abdieljrg@gmail.com>
2024-09-12 18:55:01 -0400

    fix22

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -9,7 +9,7 @@ def fizzbuzz(n):
     FizzBuzz function printss numbers from 1 to n separated by a space.
 
     - For multiples of three print "Fizz" instead of the number and for
-      multiples of five print "Buzz".
+      multiples of fivee print "Buzz".
     - For numbers which are multiples of both three and five print "FizzBuzz".
     """
     if n < 1:
